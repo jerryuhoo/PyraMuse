9683b93509fe8583cf3fde80de71e0fdc56f52cc
Yifeng Yu <yyu479@gatech.edu>
2024-03-05 16:22:44 -0500

    Update ASR model

--- rt_process.py
+++ rt_process.py
@@ -3,7 +3,7 @@ import numpy as np
 import librosa
 import pyworld
 from espnet2.bin.asr_inference import Speech2Text
-from transformers import pipeline
+from transformers import pipeline, WhisperProcessor, WhisperForConditionalGeneration
 import threading
 import queue
 import time
@@ -14,8 +14,12 @@ from pythonosc import udp_client
 Real-Time Pitch Detection and ASR
 
 This program demonstrates real-time pitch detection and automatic speech recognition (ASR) using the ESPnet2 and Hugging Face libraries.
-ASR and sentiment detection link:
+
+ASR and sentiment detection link (unused):
 https://github.com/espnet/espnet/tree/ec9760b22654dc04eeecd37e2659ebda0325a786/egs2/iemocap/asr1
+
+ASR fast model link:
+https://huggingface.co/openai/whisper-base.en
 
 Text emotion classification link:
 https://huggingface.co/michellejieli/emotion_text_classifier
@@ -36,16 +40,22 @@ osc_port = 8000  # Port number of the OSC server
 osc_client = udp_client.SimpleUDPClient(osc_ip, osc_port)
 
 # define the ASR model and text emotion classifier
-asr_model = Speech2Text.from_pretrained(
-    "espnet/YushiUeda_iemocap_sentiment_asr_train_asr_conformer_hubert"
-)
+# asr_model = Speech2Text.from_pretrained(
+#     "espnet/YushiUeda_iemocap_sentiment_asr_train_asr_conformer_hubert"
+# )
+
+# Use model from "https://huggingface.co/openai/whisper-base.en"
+processor = WhisperProcessor.from_pretrained("openai/whisper-base.en")
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-base.en")
+
+
 sentiment_classifier = pipeline(
     "sentiment-analysis", model="michellejieli/emotion_text_classifier"
 )
 
 # set the recording parameters
 pitch_duration = 0.1  # Duration for pitch detection
-asr_duration = 5  # Duration for ASR
+asr_duration = 3  # Duration for ASR
 sample_rate = 16000
 audio_queue = queue.Queue()
 asr_audio_queue = queue.Queue()
@@ -77,7 +87,7 @@ def pitch_detection():
             osc_client.send_message("/pitch", avg_audio_f0)  # Send pitch to Max/MSP
 
 
-def get_rgb_color(emo_index, sentiment_index):
+def get_rgb_color(emo_index, sentiment_index=2):
     # define the color for each emotion
     colors = {
         1: (225, 50, 50),  # anger - red
@@ -149,14 +159,24 @@ def asr_recognition():
 
         if len(asr_audio) >= asr_duration * sample_rate:
             # Speech to sentiment and text
-            text, *_ = asr_model(asr_audio)[0]
-            try:
-                sentiment, text = text.split(" ", 1)
-            except ValueError:
-                sentiment = "Neutral"
-                text = ""
-            print(f"Sentiment: {sentiment}")
-            print(f"Recognized Text: {text}")
+            # text, *_ = asr_model(asr_audio)[0]
+            # print(f"ASR Text: {text}")
+            start_time = time.time()
+            input_features = processor(
+                asr_audio, sampling_rate=sample_rate, return_tensors="pt"
+            ).input_features
+            predicted_ids = model.generate(input_features)
+            text = processor.batch_decode(predicted_ids, skip_special_tokens=True)[0]
+            print(f"ASR Time: {time.time() - start_time}")
+
+            print("ASR: ", text)
+            # try:
+            #     sentiment, text = text.split(" ", 1)
+            # except ValueError:
+            #     sentiment = "Neutral"
+            #     text = ""
+            # print(f"Sentiment: {sentiment}")
+            # print(f"Recognized Text: {text}")
 
             # Text emotion classification
             if text != "":
@@ -183,20 +203,20 @@ def asr_recognition():
                 emo_index = 5
                 output = None
 
-            if sentiment == "Neutral":
-                sentiment_index = 1
-            elif sentiment == "Positive":
-                sentiment_index = 2
-            elif sentiment == "Negative":
-                sentiment_index = 3
-            else:
-                sentiment_index = 0
+            # if sentiment == "Neutral":
+            #     sentiment_index = 1
+            # elif sentiment == "Positive":
+            #     sentiment_index = 2
+            # elif sentiment == "Negative":
+            #     sentiment_index = 3
+            # else:
+            #     sentiment_index = 0
 
-            light_color = get_rgb_color(emo_index, sentiment_index)
+            light_color = get_rgb_color(emo_index)
             print(f"Light Color: {light_color}")
 
             osc_client.send_message("/text", text)
-            osc_client.send_message("/sentiment", sentiment_index)
+            # osc_client.send_message("/sentiment", sentiment_index)
             osc_client.send_message("/emotion", emo_index)
             if output:
                 osc_client.send_message("/confidence", output[0]["score"])
@@ -246,6 +266,7 @@ def process_realtime():
 
 def process_audiofile(path):
     audio, sr = librosa.load(path, sr=sample_rate)
+
     frame_size = int(
         pitch_duration * sample_rate
     )  # Size of each frame for pitch detection
@@ -272,6 +293,9 @@ def process_audiofile(path):
                 asr_audio[:asr_frame_size]
             )  # Process the first 5s of audio for ASR
             asr_audio = asr_audio[asr_frame_size:]  # Remove the processed audio
+
+        # wait to simulate real-time processing
+        time.sleep(pitch_duration)
 
 
 # Modified pitch detection function
